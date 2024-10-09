1fd25fa4c3211f6cb3fa2d3bd607e225a3d2b213
PatelPratik39 <ptl.pratik717@gmail.com>
2024-10-09 01:07:15 -0500

    create Comments

--- client/posts/PostLists.jsx
+++ client/posts/PostLists.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import CommentCreate from "../src/CommentCreate";
 
 function PostLists() {
   const [posts, setPosts] = useState({});
@@ -24,7 +25,9 @@ function PostLists() {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentCreate postId={post.id} />
         </div>
+        
       </div>
     );
   });
--- client/src/CommentCreate.jsx
+++ client/src/CommentCreate.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
-const CommentCreate = () => {
+const CommentCreate = ({ postId }) => {
+  const [content, setConnet] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content
+    });
+    console.log(content);
+    setConnet("");
+  };
+
   return (
-    <div>CommentCreate</div>
-  )
-}
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label> New Comment </label>
+            <input
+              type="text"
+              value={content}
+              onChange={(e) => setConnet(e.target.value)}
+              className="form-control"
+            />
+          </div>
+          <button className="btn btn-primary"> Submit</button>
+        </form>
+      </div>
+    </>
+  );
+};
 
-export default CommentCreate
+export default CommentCreate;
